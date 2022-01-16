e21bfc2cf44a3246cf1e9c902ef2827b690a2f38
Niksune <nico.benoit@gmail.com>
2022-01-16 17:16:26 +0100

    Adds DeleteAccount and updates URLs

--- src/main/java/net/niksune/NixArena/Web/controllers/MainWebController.java
+++ src/main/java/net/niksune/NixArena/Web/controllers/MainWebController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://127.0.0.1:5500/actual-game/", "http://127.0.0.1:5500/general-management/"})
 public class MainWebController {
 
     @Autowired
@@ -28,13 +28,19 @@ public class MainWebController {
 
 
     @GetMapping("/allAccountsComplete")
-    public Iterable<Account> getAllAccountsComplete() { return accountRepositoryInterface.findAllCompleteBy(); }
+    public Iterable<Account> getAllAccountsComplete() {
+        return accountRepositoryInterface.findAllCompleteBy();
+    }
 
     @GetMapping("/allAccountsWithCharacs")
-    public Iterable<Account> getAllAccountsWithCharacs() { return accountRepositoryInterface.findAllWithCharacsBy(); }
+    public Iterable<Account> getAllAccountsWithCharacs() {
+        return accountRepositoryInterface.findAllWithCharacsBy();
+    }
 
     @GetMapping("/allAccountsInfos")
-    public Iterable<Account> getAllAccountsInfos() { return accountRepositoryInterface.findAllInfosBy(); }
+    public Iterable<Account> getAllAccountsInfos() {
+        return accountRepositoryInterface.findAllInfosBy();
+    }
 
     @GetMapping("/accountInfos/{id}")
     public Optional<Account> getAccountInfosById(@PathVariable("id") String id) {
@@ -47,44 +53,51 @@ public class MainWebController {
     }
 
     @GetMapping("/allCharacs")
-    public Iterable<Charac> getAllCharacs() { return characRepositoryInterface.findAll(); }
+    public Iterable<Charac> getAllCharacs() {
+        return characRepositoryInterface.findAll();
+    }
 
 
     @GetMapping("/mainScenario")
     public String mainScenario() {
 
-        Account nix = new Account("autre@gmail.com","Autre");
+        Account nix = new Account("autre@gmail.com", "Autre");
         Charac enzo = new Charac("Mathieu");
         Weapon rapier = new Weapon("Spear", 5);
         enzo.setWeaponEquipped(rapier);
         nix.getCharacters().add(enzo);
         accountRepositoryInterface.save(nix);
-        return("Account added with one armed character !");
+        return ("Account added with one armed character !");
 
     }
 
     @GetMapping("/makeWeapons")
     public String makeWeapons() {
 
-        for(int i = 1; i<11;i++)
-        {
-            Weapon w = new Weapon("Tooth Pick",i);
+        for (int i = 1; i < 11; i++) {
+            Weapon w = new Weapon("Tooth Pick", i);
             weaponRepositoryInterface.save(w);
 
         }
 
-        return("Weapons Created");
+        return ("Weapons Created");
 
     }
 
 
-    @PostMapping("/createAccount")
-    public int postAccount(@RequestBody Account account){
-        System.out.println("Ajout de : "+account);
+    @PostMapping("/accounts")
+    public int postAccount(@RequestBody Account account) {
+        System.out.println("Ajout de : " + account);
         accountRepositoryInterface.save(account);
         return 1;
     }
 
+
+    @DeleteMapping("/accounts/{id}")
+    public int deleteAccountById(@PathVariable("id") String id) {
+        accountRepositoryInterface.deleteById(Integer.parseInt(id));
+        return 1;
+    }
 
 
 }
